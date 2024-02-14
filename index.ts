fd727a4aca16aab695a23104248f4e5c82564f09
Himanshu Sharma <sharmahimanshu0405@gmail.com>
2024-02-14 18:35:17 +0530

    fix: 🐛 exporting configure and error modules

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
 export { default as RedisRPCProvider } from './providers/redis_rpc_provider.js'
+export { configure } from './configure.js'
+export { stubsRoot } from './stubs/main.js'
+export * as errors from './src/errors.js'
